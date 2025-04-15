71853fc0a8cfc35d82259822a5c15106c5488b2a
swa1245 <swarajpatil2123@gmail.com>
2025-04-15 22:46:27 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,53 @@
+// let a = 10;
+// let b = 10;
+// let sum = a + b;
+// console.log(sum);
+
+// concetation
+// let a = 12
+// let b = "132"
+// console.log(a+b);
+// console.log(typeof(a+b));  // type
+
+// template literals
+// let a =10
+// let b  = 10
+// console.log(`sum of a and b is ${a+b}`);
+
+// v=check its ans is differnt
+// ans is 1010 = string + number
+// let a =10
+// let b  = 10
+// console.log("sum of a and b " + a +b);
+
+// type coersion
+// console.log("1"-1); 0
+// console.log("1"*1); 1
+// console.log("1"/1); 1
+
+// accept and print ans
+// static means we give by our choose
+// nontstaic means user will provide
+// let age = Number(prompt("what is your age")) // to take input type casting
+// console.log(age);
+
+// swap1
+// let a = 10;
+// let b = 20;
+// let temp = a;
+// a = b;
+// b = temp;
+// console.log(a,b)
+
+// swap2
+// let a = 10
+// let b = 20
+// a = a+b // 30
+// b = a-b // 10
+// a = a - b // 20
+// console.log(a,b)
+
+// swap3
+// let [a, b] = [10, 20];
+// [a, b] = [b, a]
+// console.log(a,b)
