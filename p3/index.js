e53588b4db3e2b6d5f5cef4d0e35a4f8b80f8427
swa1245 <swarajpatil2123@gmail.com>
2025-04-21 09:27:33 +0530

    done

---
+++ p3/index.js
@@ -0,0 +1,23 @@
+// loop
+// sum
+// n = 5;
+// let sum = 0;
+// if (n >= 0) {
+//   for (var i = 0; i <= n; i++) {
+//     sum += i;
+//   }
+// }
+// console.log(sum);
+
+// fact 
+// n =5
+// let fact =1
+// for(var i=1;i<=n;i++){
+//     fact = fact *i
+// }
+// console.log(fact);
+
+// prime number
+
+n = 17
+
