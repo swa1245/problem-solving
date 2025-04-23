5ff1f0cb5b80abbadad8e690a305f5aea8a73790
swa1245 <swarajpatil2123@gmail.com>
2025-04-23 15:14:44 +0530

    done

---
+++ arrays/index.js
@@ -0,0 +1,14 @@
+// Array = colection of values such as number , strings , objects
+
+// let arr = []
+// arr.push(100)
+// arr.push("sa")
+// console.log(arr);
+
+// let arr = new Array()
+
+let arr = [1, 2, 45, 5];
+sum = 0
+for (let i = 0; i < arr.length; i++) {
+  console.log(arr[i]);
+}
